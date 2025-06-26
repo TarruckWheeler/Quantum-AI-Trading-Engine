0198e1886661497e57730d1cfc1872cfb0bbc0ca
N.E.T <143912452+TarruckWheeler@users.noreply.github.com>
2025-06-26 15:39:32 -0400

    Update __init__.py

---
+++ src/backtesting/__init__.py
@@ -0,0 +1,13 @@
+"""Backtesting and Simulation Module"""
+
+from .framework import (
+    BacktestingEngine,
+    BacktestOptimizer,
+    BacktestVisualizer
+)
+
+__all__ = [
+    "BacktestingEngine",
+    "BacktestOptimizer",
+    "BacktestVisualizer"
+]
